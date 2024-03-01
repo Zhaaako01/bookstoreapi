1b6ea8fbc036c1bcaa4f2064d399917217c943bf
Zhaaako01 <zhaako01@gmail.com>
2024-03-01 11:04:07 -0500

    1. Corrected URL for requests 2. Build HTTP statutes 3. Processed errors with try catch 4. Tested with Postman

--- src/main/java/com/bookstore/bookstoreapi/controller/APIControllers.java
+++ src/main/java/com/bookstore/bookstoreapi/controller/APIControllers.java
@@ -3,6 +3,8 @@ package com.bookstore.bookstoreapi.controller;
 import com.bookstore.bookstoreapi.models.Book;
 import com.bookstore.bookstoreapi.repo.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,37 +16,77 @@ public class APIControllers {
     private BookRepository bookRepo;
 
     @GetMapping(value = "api/books")
-    public List<Book> getAllBooks() {
-        return bookRepo.findAll();
+    public ResponseEntity<List<Book>> getAllBooks() {
+
+        try {
+            List<Book> bookList = bookRepo.findAll();
+            return ResponseEntity.status(HttpStatus.OK).body(bookList);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
-    @PostMapping(value = "api/save")
-    public String saveBook(@RequestBody Book book) {
-        bookRepo.save(book);
-        return "Book saved to the DataBase";
+    @PostMapping(value = "api/books")
+    public ResponseEntity<Book> saveBook(@RequestBody Book book) {
+
+        try {
+            Book book1 = bookRepo.save(book);
+            return ResponseEntity.status(HttpStatus.CREATED).body(book1);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+
     }
 
-    @PutMapping(value = "api/update/{id}")
-    public String updateBook(@PathVariable int id, @RequestBody Book book) {
-        Book updatedBook = bookRepo.getReferenceById(id);
-        updatedBook.setName(book.getName());
-        updatedBook.setAuthor(book.getAuthor());
-        updatedBook.setAmountOfPages(book.getAmountOfPages());
-        updatedBook.setPrice(book.getPrice());
-        bookRepo.save(updatedBook);
-        return "Book updated";
+    @PutMapping(value = "api/books/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable int id, @RequestBody Book book) {
+
+        try {
+            Optional<Book> optionalBook = bookRepo.findById(id);
+            if (optionalBook.isPresent()) {
+                Book updatedBook = bookRepo.getReferenceById(id);
+                updatedBook.setName(book.getName());
+                updatedBook.setAuthor(book.getAuthor());
+                updatedBook.setAmountOfPages(book.getAmountOfPages());
+                updatedBook.setPrice(book.getPrice());
+                Book book1 = bookRepo.save(updatedBook);
+                return ResponseEntity.status(HttpStatus.OK).body(book1);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
     }
 
-    @DeleteMapping(value = "api/delete/{id}")
-    public String deleteBook(@PathVariable int id) {
-        Book deletedBook = bookRepo.getReferenceById(id);
-        bookRepo.delete(deletedBook);
-        return "Book by the id: " + id + " deleted";
+    @DeleteMapping(value = "api/books/{id}")
+    public ResponseEntity<Optional<Book>> deleteBook(@PathVariable int id) {
+
+        try {
+            Optional<Book> deletedBook = bookRepo.findById(id);
+            if (deletedBook.isPresent()) {
+                bookRepo.deleteById(id);
+                return ResponseEntity.status(HttpStatus.OK).body(deletedBook);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping(value = "api/books/{id}")
-    public Book getBookByID(@PathVariable int id) {
-        Optional<Book> optionalBook = bookRepo.findById(id);
-        return optionalBook.orElse(null);
+    public ResponseEntity<Optional<Book>> getBookByID(@PathVariable int id) {
+
+        try {
+            Optional<Book> optionalBook = bookRepo.findById(id);
+            if (optionalBook.isPresent()) {
+                return ResponseEntity.status(HttpStatus.OK).body(optionalBook);
+            } else return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
